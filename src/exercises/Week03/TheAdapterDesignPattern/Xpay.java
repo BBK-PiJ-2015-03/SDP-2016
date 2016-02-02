03a6672efdd361c5fb43dc0def2c5aea1d253d12
BBK-PiJ-2015-03 <sales@zozeri.com>
2016-02-02 15:42:10 +0000

    a start on the adapter pattern

---
+++ src/exercises/Week03/TheAdapterDesignPattern/Xpay.java
@@ -0,0 +1,19 @@
+package exercises.Week03.TheAdapterDesignPattern;
+
+/**
+ * Created by J2FX on 02/02/2016.
+ */
+public interface Xpay {
+    String getCreditCardNo();
+    void setCreditCardNo(String creditCardNo);
+    String getCustomerName();
+    void setCustomerName(String customerName);
+    String getCardExpMonth();
+    void setCardExpMonth(String cardExpMonth);
+    String getCardExpYear();
+    void setCardExpYear(String cardExpYear);
+    Short getCardCVVNo();
+    void setCardCVVNo(Short cardCVVNo);
+    Double getAmount();
+    void setAmount(Double amount);
+}
---
+++ src/exercises/Week03/TheAdapterDesignPattern/XpayToPayDAdapter.java
@@ -0,0 +1,61 @@
+package exercises.Week03.TheAdapterDesignPattern;
+
+/**
+ * Adapter class
+ *
+ * I would assume that in the reality this
+ * class would override XPayImpl as new objects
+ * of XPayImpl are created in other classes.
+ */
+/*
+public class XpayToPayDAdapter {
+
+    public String getCreditCardNo() {
+        return creditCardNo;
+    }
+
+    public void setCreditCardNo(String creditCardNo) {
+        this.creditCardNo = creditCardNo;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCardExpMonth() {
+        return cardExpMonth;
+    }
+
+    public void setCardExpMonth(String cardExpMonth) {
+        this.cardExpMonth = cardExpMonth;
+    }
+
+    public String getCardExpYear() {
+        return cardExpYear;
+    }
+
+    public void setCardExpYear(String cardExpYear) {
+        this.cardExpYear = cardExpYear;
+    }
+
+    public Short getCardCVVNo() {
+        return cardCVVNo;
+    }
+
+    public void setCardCVVNo(Short cardCVVNo) {
+        this.cardCVVNo = cardCVVNo;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+}
+*/
